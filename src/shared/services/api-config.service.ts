09baacb3c94dd5e1b03791afca637a1b714f5202
er-hungn <hungn@tech.est-rouge.com>
2024-01-22 15:32:46 +0700

    [RSMPT3-25] Setup logger

--- src/shared/services/api-config.service.ts
+++ src/shared/services/api-config.service.ts
@@ -6,16 +6,68 @@ import { type TypeOrmModuleOptions } from '@nestjs/typeorm';
 export class ApiConfigService {
   constructor(private configService: ConfigService) {}
 
+  get isDevelopment(): boolean {
+    return this.nodeEnv === 'development';
+  }
+
+  get isProduction(): boolean {
+    return this.nodeEnv === 'production';
+  }
+
+  get isTest(): boolean {
+    return this.nodeEnv === 'test';
+  }
+
+  private getNumber(key: string): number {
+    const value = this.get(key);
+
+    try {
+      return Number(value);
+    } catch {
+      throw new Error(key + ' environment variable is not a number');
+    }
+  }
+
+  private getBoolean(key: string): boolean {
+    const value = this.get(key);
+
+    try {
+      return Boolean(JSON.parse(value));
+    } catch {
+      throw new Error(key + ' env var is not a boolean');
+    }
+  }
+
+  private getString(key: string): string {
+    const value = this.get(key);
+
+    return value.replaceAll('\\n', '\n');
+  }
+
+  get nodeEnv(): string {
+    return this.getString('NODE_ENV');
+  }
+
   get mysqlConfig(): TypeOrmModuleOptions {
     return {
       type: 'mysql',
-      host: this.configService.get('MYSQL_HOST'),
-      port: +this.configService.get('MYSQL_PORT'),
-      username: this.configService.get('MYSQL_USER'),
-      password: this.configService.get('MYSQL_PASSWORD'),
-      database: this.configService.get('MYSQL_DATABASE'),
+      host: this.getString('MYSQL_HOST'),
+      port: this.getNumber('MYSQL_PORT'),
+      username: this.getString('MYSQL_USER'),
+      password: this.getString('MYSQL_PASSWORD'),
+      database: this.getString('MYSQL_DATABASE'),
       entities: [],
       synchronize: true,
     };
+  }
+
+  private get(key: string): string {
+    const value = this.configService.get<string>(key);
+
+    if (!value) {
+      throw new Error(key + ' environment variable does not set');
+    }
+
+    return value;
   }
 }
